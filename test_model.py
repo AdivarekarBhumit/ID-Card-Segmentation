932816aa517fafaa866205a1b29795a0fcaab06d
AdivarekarBhumit <bhumit97ad@gmail.com>
2019-01-26 23:04:19 +0530

    Minor Fixes:alembic:

--- test_model.py
+++ test_model.py
@@ -3,19 +3,19 @@ import numpy as np
 from keras.models import load_model
 from keras.optimizers import Adam
 from model.iou_loss import IoU
+import matplotlib.pyplot as plt
 
 model = load_model('unet_model_whole_100epochs.h5', compile=False)
 model.compile(optimizer=Adam(1e-4), loss=IoU, metrics=['binary_accuracy'])
 
-image_name = 'test.tif'
+image_name = './images/test.tif'
 
 img = cv2.imread(image_name)
+h,w = img.shape[:2]
 img = cv2.resize(img, (256,256))
 img = img / 255.0
 predict = model.predict(img.reshape(1,256,256,3))
 
 output = predict[0]
-
-cv2.imshow('Output', output)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+output = cv2.resize(output, (w,h))
+plt.imsave('output.jpg', output, cmap='gray')
