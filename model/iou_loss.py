0baa63dcf3d6c781fd5bb837248291dd20f714c2
AdivarekarBhumit <bhumit97ad@gmail.com>
2019-01-26 22:13:43 +0530

    IoU Loss:art:

---
+++ model/iou_loss.py
@@ -0,0 +1,12 @@
+'''
+Intersection Over Union Loss
+'''
+import numpy as np
+from keras import backend as K
+
+def IoU(y_true, y_pred, eps=1e-6):
+    if np.max(y_true) == 0.0:
+        return IoU(1-y_true, 1-y_pred) ## empty image; calc IoU of zeros
+    intersection = K.sum(y_true * y_pred, axis=[1,2,3])
+    union = K.sum(y_true, axis=[1,2,3]) + K.sum(y_pred, axis=[1,2,3]) - intersection
+    return -K.mean( (intersection + eps) / (union + eps), axis=0)
