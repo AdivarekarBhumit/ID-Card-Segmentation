922e09aac9d7a4b5b9c8ca0a72bc10d31f73ec7c
AdivarekarBhumit <bhumit97ad@gmail.com>
2019-01-26 22:22:35 +0530

    Renamed FileName to avoid confusion:truck:

---
+++ model/unet_model.py
@@ -0,0 +1,63 @@
+from keras.models import Model, load_model
+from keras.layers import Conv2D, MaxPool2D, Input, concatenate, UpSampling2D, Dropout
+from keras.layers import Conv2DTranspose, AvgPool2D, GaussianNoise, BatchNormalization
+import tensorflow as tf
+import keras.backend as K
+from keras.optimizers import Adam
+from keras.losses import binary_crossentropy
+import keras
+from keras.callbacks import TensorBoard, ModelCheckpoint
+from .iou_loss import IoU
+
+def get_model():
+    K.clear_session()
+    inputs = Input((256,256,3))
+    s = BatchNormalization()(inputs) # we can learn the normalization step
+    s = Dropout(0.5)(s)
+
+    c1 = Conv2D(8, (3, 3), activation='relu', padding='same') (s)
+    c1 = Conv2D(8, (3, 3), activation='relu', padding='same') (c1)
+    p1 = MaxPool2D((2, 2)) (c1)
+
+    c2 = Conv2D(16, (3, 3), activation='relu', padding='same') (p1)
+    c2 = Conv2D(16, (3, 3), activation='relu', padding='same') (c2)
+    p2 = MaxPool2D((2, 2)) (c2)
+
+    c3 = Conv2D(32, (3, 3), activation='relu', padding='same') (p2)
+    c3 = Conv2D(32, (3, 3), activation='relu', padding='same') (c3)
+    p3 = MaxPool2D((2, 2)) (c3)
+
+    c4 = Conv2D(64, (3, 3), activation='relu', padding='same') (p3)
+    c4 = Conv2D(64, (3, 3), activation='relu', padding='same') (c4)
+    p4 = MaxPool2D(pool_size=(2, 2)) (c4)
+
+    c5 = Conv2D(128, (3, 3), activation='relu', padding='same') (p4)
+    c5 = Conv2D(128, (3, 3), activation='relu', padding='same') (c5)
+
+    u6 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same') (c5)
+    u6 = concatenate([u6, c4])
+    c6 = Conv2D(64, (3, 3), activation='relu', padding='same') (u6)
+    c6 = Conv2D(64, (3, 3), activation='relu', padding='same') (c6)
+
+    u7 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same') (c6)
+    u7 = concatenate([u7, c3])
+    c7 = Conv2D(32, (3, 3), activation='relu', padding='same') (u7)
+    c7 = Conv2D(32, (3, 3), activation='relu', padding='same') (c7)
+
+    u8 = Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same') (c7)
+    u8 = concatenate([u8, c2])
+    c8 = Conv2D(16, (3, 3), activation='relu', padding='same') (u8)
+    c8 = Conv2D(16, (3, 3), activation='relu', padding='same') (c8)
+
+    u9 = Conv2DTranspose(8, (2, 2), strides=(2, 2), padding='same') (c8)
+    u9 = concatenate([u9, c1], axis=3)
+    c9 = Conv2D(8, (3, 3), activation='relu', padding='same') (u9)
+    c9 = Conv2D(8, (3, 3), activation='relu', padding='same') (c9)
+
+    outputs = Conv2D(1, (1, 1), activation='sigmoid') (c9)
+
+    model = Model(inputs=inputs, outputs=outputs)
+    print(model.summary())
+    model.compile(optimizer=Adam(lr=1e-4), loss=IoU, metrics=['binary_accuracy'])
+
+    return model
